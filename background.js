7759f1cd0a86c9c098214e6424ca3f46a9ce5806
Hang Luong <hpl7@duke.edu>
2025-03-02 02:19:50 +0800

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "getCookies") {
-    chrome.cookies.getAll({ url: request.url }, (cookies) => {
-      sendResponse({ cookies: cookies || [] });
-    });
-    return true; // Keeps the message channel open for asynchronous response
-  }
-});
